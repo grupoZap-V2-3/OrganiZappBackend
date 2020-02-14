f417731df586e5b65da42967869a670d132bef6b
Steve <Steve@cafesito.com>
2020-02-13 22:04:46 -0600

    Cambie mensaje de respuesta

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,6 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+    response.send("Hola from Firebase!");
 });
 
